a8cb8d0d9f00f117d08da7306f690341ddc239f9
Mario Andres Morales Moreno <6469036+emetricz@users.noreply.github.com>
2023-12-20 08:03:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
+import numpy as np
 
 def plot_data(dataframes, offsets):
     fig = go.Figure()
@@ -10,7 +11,7 @@ def plot_data(dataframes, offsets):
         for col in selected_columns:
             x_offset = offsets.get((file_name, col), {}).get('x', 0)
             y_offset = offsets.get((file_name, col), {}).get('y', 0)
-            fig.add_trace(go.Scatter(x=df.index + x_offset, y=df[col] + y_offset, mode='lines', name=f"{file_name} - {col}"))
+            fig.add_trace(go.Scatter(x=df.index + x_offset, y=df[col] + y_offset, mode='lines+markers', name=f"{file_name} - {col}"))
 
     fig.update_layout(
         xaxis_title="X Axis",
@@ -25,8 +26,11 @@ def plot_data(dataframes, offsets):
     fig.update_layout(hovermode='x')
     return fig
 
+def calculate_distance(point1, point2):
+    return np.sqrt((point2[0] - point1[0])**2 + (point2[1] - point1[1])**2)
+
 # Streamlit App
-st.title("Interactive Multi-Line Data Visualization with Individual Offsets")
+st.title("Interactive Multi-Line Data Visualization with Distance Measurement")
 
 # File Uploader for multiple files
 uploaded_files = st.file_uploader("Choose CSV files", type="csv", accept_multiple_files=True)
@@ -44,28 +48,32 @@ if uploaded_files:
         selected_columns = st.multiselect(f"Select columns to plot from {uploaded_file.name}", df.columns)
         dataframes[uploaded_file.name] = (df, selected_columns)
 
-        # Set offsets for each line
-        for col in selected_columns:
-            col1, col2 = st.columns(2)
-            with col1:
-                x_offset = st.slider(f"Set X Offset for {col} in {uploaded_file.name}", min_value=-100, max_value=100, value=0, key=f"x_{uploaded_file.name}_{col}")
-            with col2:
-                y_offset = st.slider(f"Set Y Offset for {col} in {uploaded_file.name}", min_value=-100, max_value=100, value=0, key=f"y_{uploaded_file.name}_{col}")
-            
-            offsets[(uploaded_file.name, col)] = {'x': x_offset, 'y': y_offset}
-
     if st.button("Plot Data"):
         fig = plot_data(dataframes, offsets)
         st.plotly_chart(fig, use_container_width=True)
 
-    # Save, Print, Reset Buttons
-    col1, col2, col3 = st.columns(3)
-    if col1.button("Save Graph"):
-        fig.write_image("plot.png")
-        st.success("Saved as plot.png")
+# Distance Measurement
+st.write("Distance Measurement:")
+col1, col2 = st.columns(2)
+with col1:
+    point1_x = st.number_input("Point 1 X", key="p1x")
+    point1_y = st.number_input("Point 1 Y", key="p1y")
+with col2:
+    point2_x = st.number_input("Point 2 X", key="p2x")
+    point2_y = st.number_input("Point 2 Y", key="p2y")
 
-    if col2.button("Print Graph"):
-        fig.show(renderer="browser")
+if st.button("Calculate Distance"):
+    distance = calculate_distance((point1_x, point1_y), (point2_x, point2_y))
+    st.write(f"Distance: {distance}")
 
-    if col3.button("Reset Graph"):
-        st.experimental_rerun()
+# Save, Print, Reset Buttons
+col1, col2, col3 = st.columns(3)
+if col1.button("Save Graph"):
+    fig.write_image("plot.png")
+    st.success("Saved as plot.png")
+
+if col2.button("Print Graph"):
+    fig.show(renderer="browser")
+
+if col3.button("Reset Graph"):
+    st.experimental_rerun()
