db4a9dcde6e725d6270f0b5028e40fb31dc3da87
Mario Andres Morales Moreno <6469036+emetricz@users.noreply.github.com>
2023-12-20 07:28:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,11 @@ def plot_data(dataframes, offsets):
         for col in selected_columns:
             x_offset = offsets.get((file_name, col), {}).get('x', 0)
             y_offset = offsets.get((file_name, col), {}).get('y', 0)
-            fig.add_trace(go.Scatter(x=df.index + x_offset, y=df[col] + y_offset, mode='lines+markers', name=f"{file_name} - {col}"))
+            fig.add_trace(go.Scatter(x=df.index + x_offset, y=df[col] + y_offset, mode='lines', name=f"{file_name} - {col}"))
 
     fig.update_layout(
         xaxis_title="X Axis",
         yaxis_title="Y Axis",
-        hovermode='closest',
         margin=dict(l=0, r=0, t=0, b=0),
         xaxis=dict(showgrid=True, zeroline=True, showticklabels=True),
         yaxis=dict(showgrid=True, zeroline=True, showticklabels=True),
@@ -23,10 +22,11 @@ def plot_data(dataframes, offsets):
     )
 
     fig.update_xaxes(rangeslider_visible=True)
+    fig.update_layout(hovermode='x')
     return fig
 
 # Streamlit App
-st.title("Interactive Multi-Line Data Visualization with Point Selection")
+st.title("Interactive Multi-Line Data Visualization with Individual Offsets")
 
 # File Uploader for multiple files
 uploaded_files = st.file_uploader("Choose CSV files", type="csv", accept_multiple_files=True)
@@ -48,9 +48,9 @@ if uploaded_files:
         for col in selected_columns:
             col1, col2 = st.columns(2)
             with col1:
-                x_offset = st.slider(f"X Offset for {col} in {uploaded_file.name}", min_value=-100, max_value=100, value=0, key=f"x_{uploaded_file.name}_{col}")
+                x_offset = st.slider(f"Set X Offset for {col} in {uploaded_file.name}", min_value=-100, max_value=100, value=0, key=f"x_{uploaded_file.name}_{col}")
             with col2:
-                y_offset = st.slider(f"Y Offset for {col} in {uploaded_file.name}", min_value=-100, max_value=100, value=0, key=f"y_{uploaded_file.name}_{col}")
+                y_offset = st.slider(f"Set Y Offset for {col} in {uploaded_file.name}", min_value=-100, max_value=100, value=0, key=f"y_{uploaded_file.name}_{col}")
             
             offsets[(uploaded_file.name, col)] = {'x': x_offset, 'y': y_offset}
 
@@ -58,25 +58,14 @@ if uploaded_files:
         fig = plot_data(dataframes, offsets)
         st.plotly_chart(fig, use_container_width=True)
 
-# Manual Input for Point Selection
-col1, col2 = st.columns(2)
-with col1:
-    point1_x = st.number_input("Point 1 X Value", value=0)
-    point1_y = st.number_input("Point 1 Y Value", value=0)
+    # Save, Print, Reset Buttons
+    col1, col2, col3 = st.columns(3)
+    if col1.button("Save Graph"):
+        fig.write_image("plot.png")
+        st.success("Saved as plot.png")
 
-with col2:
-    point2_x = st.number_input("Point 2 X Value", value=0)
-    point2_y = st.number_input("Point 2 Y Value", value=0)
+    if col2.button("Print Graph"):
+        fig.show(renderer="browser")
 
-# Calculate and Display Differences
-if st.button("Calculate Difference"):
-    diff_x = point2_x - point1_x
-    diff_y = point2_y - point1_y
-    st.write(f"Difference in X: {diff_x}")
-    st.write(f"Difference in Y: {diff_y}")
-
-# Save, Print, Reset Buttons
-col1, col2, col3 = st.columns(3)
-if col1.button("Save Graph"):
-    fig.write_image("plot.png")
-    st.success("Saved as plot.png")
+    if col3.button("Reset Graph"):
+        st.experimental_rerun()
