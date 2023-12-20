074739a74d1575ef811941f1b5a5fccee1172120
Mario Andres Morales Moreno <6469036+emetricz@users.noreply.github.com>
2023-12-20 07:21:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,7 @@ def plot_data(dataframes, offsets):
     fig.update_layout(
         xaxis_title="X Axis",
         yaxis_title="Y Axis",
+        hovermode='closest',
         margin=dict(l=0, r=0, t=0, b=0),
         xaxis=dict(showgrid=True, zeroline=True, showticklabels=True),
         yaxis=dict(showgrid=True, zeroline=True, showticklabels=True),
@@ -22,18 +23,16 @@ def plot_data(dataframes, offsets):
     )
 
     fig.update_xaxes(rangeslider_visible=True)
-    fig.update_layout(hovermode='x')
     return fig
 
 # Streamlit App
-st.title("Interactive Multi-Line Data Visualization with Markers and Differences")
+st.title("Interactive Multi-Line Data Visualization with Point Selection")
 
 # File Uploader for multiple files
 uploaded_files = st.file_uploader("Choose CSV files", type="csv", accept_multiple_files=True)
 
 dataframes = {}
 offsets = {}
-differences = {}
 
 if uploaded_files:
     for uploaded_file in uploaded_files:
@@ -45,9 +44,9 @@ if uploaded_files:
         selected_columns = st.multiselect(f"Select columns to plot from {uploaded_file.name}", df.columns)
         dataframes[uploaded_file.name] = (df, selected_columns)
 
-        # Set offsets and select points for each line
+        # Set offsets for each line
         for col in selected_columns:
-            col1, col2, col3 = st.columns(3)
+            col1, col2 = st.columns(2)
             with col1:
                 x_offset = st.slider(f"X Offset for {col} in {uploaded_file.name}", min_value=-100, max_value=100, value=0, key=f"x_{uploaded_file.name}_{col}")
             with col2:
@@ -55,28 +54,29 @@ if uploaded_files:
             
             offsets[(uploaded_file.name, col)] = {'x': x_offset, 'y': y_offset}
 
-            with col3:
-                point1 = st.number_input(f"Point 1 for {col} in {uploaded_file.name}", min_value=0, max_value=len(df)-1, key=f"p1_{uploaded_file.name}_{col}")
-                point2 = st.number_input(f"Point 2 for {col} in {uploaded_file.name}", min_value=0, max_value=len(df)-1, key=f"p2_{uploaded_file.name}_{col}")
-                if point1 != point2:
-                    diff_x = df.index[point2] - df.index[point1]
-                    diff_y = df[col][point2] - df[col][point1]
-                    differences[(uploaded_file.name, col)] = {'Point 1': point1, 'Point 2': point2, 'X Difference': diff_x, 'Y Difference': diff_y}
-
     if st.button("Plot Data"):
         fig = plot_data(dataframes, offsets)
         st.plotly_chart(fig, use_container_width=True)
 
-    # Display differences in a table
-    if differences:
-        st.write("Differences between selected points:")
-        st.table(pd.DataFrame.from_dict(differences, orient='index'))
+# Manual Input for Point Selection
+col1, col2 = st.columns(2)
+with col1:
+    point1_x = st.number_input("Point 1 X Value", value=0)
+    point1_y = st.number_input("Point 1 Y Value", value=0)
 
-    # Save, Print, Reset Buttons
-    col1, col2, col3 = st.columns(3)
-    if col1.button("Save Graph"):
-        fig.write_image("plot.png")
-        st.success("Saved as plot.png")
+with col2:
+    point2_x = st.number_input("Point 2 X Value", value=0)
+    point2_y = st.number_input("Point 2 Y Value", value=0)
 
-    if col2.button("Print Graph"):
-        fig.show(renderer="browser")
+# Calculate and Display Differences
+if st.button("Calculate Difference"):
+    diff_x = point2_x - point1_x
+    diff_y = point2_y - point1_y
+    st.write(f"Difference in X: {diff_x}")
+    st.write(f"Difference in Y: {diff_y}")
+
+# Save, Print, Reset Buttons
+col1, col2, col3 = st.columns(3)
+if col1.button("Save Graph"):
+    fig.write_image("plot.png")
+    st.success("Saved as plot.png")
