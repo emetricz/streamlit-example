652875df4b3cd962a75cdd699904ae7828772ea3
Mario Andres Morales Moreno <6469036+emetricz@users.noreply.github.com>
2023-12-20 08:14:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,21 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
-import numpy as np
 
-def plot_data(dataframes, offsets):
+def plot_data(dataframes, offsets, line_positions):
     fig = go.Figure()
 
+    # Plotting the data
     for file_name, df_info in dataframes.items():
         df, selected_columns = df_info
         for col in selected_columns:
             x_offset = offsets.get((file_name, col), {}).get('x', 0)
             y_offset = offsets.get((file_name, col), {}).get('y', 0)
-            fig.add_trace(go.Scatter(x=df.index + x_offset, y=df[col] + y_offset, mode='lines+markers', name=f"{file_name} - {col}"))
+            fig.add_trace(go.Scatter(x=df.index + x_offset, y=df[col] + y_offset, mode='lines', name=f"{file_name} - {col}"))
+
+    # Adding horizontal lines
+    for line_y in line_positions:
+        fig.add_hline(y=line_y, line_dash="dot", annotation_text=f"y={line_y}", annotation_position="bottom right")
 
     fig.update_layout(
         xaxis_title="X Axis",
@@ -26,17 +30,15 @@ def plot_data(dataframes, offsets):
     fig.update_layout(hovermode='x')
     return fig
 
-def calculate_distance(point1, point2):
-    return np.sqrt((point2[0] - point1[0])**2 + (point2[1] - point1[1])**2)
-
 # Streamlit App
-st.title("Interactive Multi-Line Data Visualization with Distance Measurement")
+st.title("Interactive Multi-Line Data Visualization with Individual Offsets and Horizontal Lines")
 
 # File Uploader for multiple files
 uploaded_files = st.file_uploader("Choose CSV files", type="csv", accept_multiple_files=True)
 
 dataframes = {}
 offsets = {}
+line_positions = []
 
 if uploaded_files:
     for uploaded_file in uploaded_files:
@@ -48,23 +50,25 @@ if uploaded_files:
         selected_columns = st.multiselect(f"Select columns to plot from {uploaded_file.name}", df.columns)
         dataframes[uploaded_file.name] = (df, selected_columns)
 
-    if st.button("Plot Data"):
-        fig = plot_data(dataframes, offsets)
-        st.plotly_chart(fig, use_container_width=True)
+        # Set offsets for each line
+        for col in selected_columns:
+            col1, col2 = st.columns(2)
+            with col1:
+                x_offset = st.slider(f"Set X Offset for {col} in {uploaded_file.name}", min_value=-100, max_value=100, value=0, key=f"x_{uploaded_file.name}_{col}")
+            with col2:
+                y_offset = st.slider(f"Set Y Offset for {col} in {uploaded_file.name}", min_value=-100, max_value=100, value=0, key=f"y_{uploaded_file.name}_{col}")
+            
+            offsets[(uploaded_file.name, col)] = {'x': x_offset, 'y': y_offset}
 
-# Distance Measurement
-st.write("Distance Measurement:")
-col1, col2 = st.columns(2)
-with col1:
-    point1_x = st.number_input("Point 1 X", key="p1x")
-    point1_y = st.number_input("Point 1 Y", key="p1y")
-with col2:
-    point2_x = st.number_input("Point 2 X", key="p2x")
-    point2_y = st.number_input("Point 2 Y", key="p2y")
+# Adding horizontal lines
+line_count = st.slider("Select number of horizontal lines", 0, 5, 1)
+for i in range(line_count):
+    line_y = st.number_input(f"Position of line {i+1}", key=f'line_{i}')
+    line_positions.append(line_y)
 
-if st.button("Calculate Distance"):
-    distance = calculate_distance((point1_x, point1_y), (point2_x, point2_y))
-    st.write(f"Distance: {distance}")
+if st.button("Plot Data"):
+    fig = plot_data(dataframes, offsets, line_positions)
+    st.plotly_chart(fig, use_container_width=True)
 
 # Save, Print, Reset Buttons
 col1, col2, col3 = st.columns(3)
