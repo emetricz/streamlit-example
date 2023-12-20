b142aab079eff0b1be3ceea412d35dc520563dda
Mario Andres Morales Moreno <6469036+emetricz@users.noreply.github.com>
2023-12-20 08:22:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,9 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 
-def plot_data(dataframes, offsets, line_positions):
+def plot_data(dataframes, offsets, h_lines, v_lines):
     fig = go.Figure()
 
-    # Plotting the data
     for file_name, df_info in dataframes.items():
         df, selected_columns = df_info
         for col in selected_columns:
@@ -13,9 +12,11 @@ def plot_data(dataframes, offsets, line_positions):
             y_offset = offsets.get((file_name, col), {}).get('y', 0)
             fig.add_trace(go.Scatter(x=df.index + x_offset, y=df[col] + y_offset, mode='lines', name=f"{file_name} - {col}"))
 
-    # Adding horizontal lines
-    for line_y in line_positions:
-        fig.add_hline(y=line_y, line_dash="dot", annotation_text=f"y={line_y}", annotation_position="bottom right")
+    for h_line in h_lines:
+        fig.add_hline(y=h_line, line_dash="dash", line_color="red")
+
+    for v_line in v_lines:
+        fig.add_vline(x=v_line, line_dash="dash", line_color="blue")
 
     fig.update_layout(
         xaxis_title="X Axis",
@@ -31,14 +32,15 @@ def plot_data(dataframes, offsets, line_positions):
     return fig
 
 # Streamlit App
-st.title("Interactive Multi-Line Data Visualization with Individual Offsets and Horizontal Lines")
+st.title("Interactive Multi-Line Data Visualization with Line Controls")
 
 # File Uploader for multiple files
 uploaded_files = st.file_uploader("Choose CSV files", type="csv", accept_multiple_files=True)
 
 dataframes = {}
 offsets = {}
-line_positions = []
+h_lines = []
+v_lines = []
 
 if uploaded_files:
     for uploaded_file in uploaded_files:
@@ -46,28 +48,31 @@ if uploaded_files:
         st.write(f"Data from: {uploaded_file.name}")
         st.dataframe(df.head())
 
-        # Select columns to plot
         selected_columns = st.multiselect(f"Select columns to plot from {uploaded_file.name}", df.columns)
         dataframes[uploaded_file.name] = (df, selected_columns)
 
-        # Set offsets for each line
         for col in selected_columns:
             col1, col2 = st.columns(2)
             with col1:
-                x_offset = st.slider(f"Set X Offset for {col} in {uploaded_file.name}", min_value=-100, max_value=100, value=0, key=f"x_{uploaded_file.name}_{col}")
+                x_offset = st.slider(f"Set X Offset for {col} in {uploaded_file.name}", -100, 100, 0, key=f"x_{uploaded_file.name}_{col}")
             with col2:
-                y_offset = st.slider(f"Set Y Offset for {col} in {uploaded_file.name}", min_value=-100, max_value=100, value=0, key=f"y_{uploaded_file.name}_{col}")
-            
+                y_offset = st.slider(f"Set Y Offset for {col} in {uploaded_file.name}", -100, 100, 0, key=f"y_{uploaded_file.name}_{col}")
             offsets[(uploaded_file.name, col)] = {'x': x_offset, 'y': y_offset}
 
-# Adding horizontal lines
-line_count = st.slider("Select number of horizontal lines", 0, 5, 1)
-for i in range(line_count):
-    line_y = st.number_input(f"Position of line {i+1}", key=f'line_{i}')
-    line_positions.append(line_y)
+# Horizontal and Vertical Line Controls
+st.sidebar.title("Line Controls")
+with st.sidebar:
+    st.subheader("Horizontal Lines")
+    h_line_count = st.number_input("Number of horizontal lines", 0, 5, 0)
+    h_lines = [st.slider(f"Y position of line {i+1}", -100.0, 100.0, 0.0, key=f"h_line_{i}") for i in range(h_line_count)]
 
+    st.subheader("Vertical Lines")
+    v_line_count = st.number_input("Number of vertical lines", 0, 5, 0)
+    v_lines = [st.slider(f"X position of line {i+1}", -100.0, 100.0, 0.0, key=f"v_line_{i}") for i in range(v_line_count)]
+
+# Plotting
 if st.button("Plot Data"):
-    fig = plot_data(dataframes, offsets, line_positions)
+    fig = plot_data(dataframes, offsets, h_lines, v_lines)
     st.plotly_chart(fig, use_container_width=True)
 
 # Save, Print, Reset Buttons
@@ -80,4 +85,4 @@ if col2.button("Print Graph"):
     fig.show(renderer="browser")
 
 if col3.button("Reset Graph"):
-    st.experimental_rerun()
+    st.experimental_rerun
