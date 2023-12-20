46c07e5723859cb051549cf23ea140871ea7e321
Mario Andres Morales Moreno <6469036+emetricz@users.noreply.github.com>
2023-12-20 06:30:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,14 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 
-def plot_data(data, x_origin, y_origin, columns):
+def plot_data(dataframes, line_names):
     fig = go.Figure()
 
-    for col in columns:
-        fig.add_trace(go.Scatter(x=data.index - x_origin, y=data[col] - y_origin, mode='lines', name=col))
+    for file_name, df_info in dataframes.items():
+        df, selected_columns = df_info
+        for col in selected_columns:
+            line_name = line_names.get(file_name, {}).get(col, col)
+            fig.add_trace(go.Scatter(x=df.index, y=df[col], mode='lines', name=line_name))
 
     fig.update_layout(
         xaxis_title="X Axis",
@@ -16,44 +19,39 @@ def plot_data(data, x_origin, y_origin, columns):
         yaxis=dict(showgrid=True, zeroline=True, showticklabels=True),
         dragmode='pan'
     )
-    
+
     fig.update_xaxes(rangeslider_visible=True)
     fig.update_layout(hovermode='x')
     return fig
 
 # Streamlit App
-st.title("Interactive Data Visualization with Multiple CSVs")
+st.title("Interactive Multi-Line Data Visualization")
 
 # File Uploader for multiple files
 uploaded_files = st.file_uploader("Choose CSV files", type="csv", accept_multiple_files=True)
+
 dataframes = {}
+line_names = {}
 
 if uploaded_files:
     for uploaded_file in uploaded_files:
-        # Reading each file
         df = pd.read_csv(uploaded_file)
         st.write(f"Data from: {uploaded_file.name}")
         st.dataframe(df.head())
 
-        # Allow user to rename columns
-        new_column_names = st.text_input(f"Rename columns for {uploaded_file.name} (comma-separated)", value=",".join(df.columns))
-        if new_column_names:
-            df.columns = [name.strip() for name in new_column_names.split(",")]
+        # Select columns to plot
+        selected_columns = st.multiselect(f"Select columns to plot from {uploaded_file.name}", df.columns)
+        dataframes[uploaded_file.name] = (df, selected_columns)
 
-        dataframes[uploaded_file.name] = df
+        # Rename line names
+        for col in selected_columns:
+            default_name = f"{uploaded_file.name} - {col}"
+            line_name = st.text_input(f"Rename line for {col} in {uploaded_file.name}", value=default_name)
+            if line_name:
+                line_names.setdefault(uploaded_file.name, {})[col] = line_name
 
-    # Selecting a dataframe to plot
-    file_to_plot = st.selectbox("Select a file to plot", options=list(dataframes.keys()))
-    selected_df = dataframes[file_to_plot]
-    columns_to_plot = st.multiselect("Select columns to plot", selected_df.columns)
-
-    # Axis Manipulation
-    x_origin = st.slider("Adjust X Origin", min_value=0, max_value=len(selected_df), value=0)
-    y_origin = st.slider("Adjust Y Origin", min_value=int(selected_df[columns_to_plot].min().min()), max_value=int(selected_df[columns_to_plot].max().max()), value=0)
-
-    # Plotting
-    if columns_to_plot:
-        fig = plot_data(selected_df, x_origin, y_origin, columns_to_plot)
+    if st.button("Plot Data"):
+        fig = plot_data(dataframes, line_names)
         st.plotly_chart(fig, use_container_width=True)
 
     # Save, Print, Reset Buttons
