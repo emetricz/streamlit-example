788deb0fccb01aa4df4e0e3c30c79aec7c977c76
Mario Andres Morales Moreno <6469036+emetricz@users.noreply.github.com>
2023-12-20 07:02:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,16 +2,15 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 
-def plot_data(dataframes, line_names, offsets):
+def plot_data(dataframes, offsets):
     fig = go.Figure()
 
     for file_name, df_info in dataframes.items():
         df, selected_columns = df_info
         for col in selected_columns:
-            line_name = line_names.get(file_name, {}).get(col, col)
-            x_offset = offsets.get(file_name, {}).get(col, {}).get('x', 0)
-            y_offset = offsets.get(file_name, {}).get(col, {}).get('y', 0)
-            fig.add_trace(go.Scatter(x=df.index + x_offset, y=df[col] + y_offset, mode='lines', name=line_name))
+            x_offset = offsets.get((file_name, col), {}).get('x', 0)
+            y_offset = offsets.get((file_name, col), {}).get('y', 0)
+            fig.add_trace(go.Scatter(x=df.index + x_offset, y=df[col] + y_offset, mode='lines', name=f"{file_name} - {col}"))
 
     fig.update_layout(
         xaxis_title="X Axis",
@@ -33,7 +32,6 @@ st.title("Interactive Multi-Line Data Visualization with Individual Offsets")
 uploaded_files = st.file_uploader("Choose CSV files", type="csv", accept_multiple_files=True)
 
 dataframes = {}
-line_names = {}
 offsets = {}
 
 if uploaded_files:
@@ -46,25 +44,18 @@ if uploaded_files:
         selected_columns = st.multiselect(f"Select columns to plot from {uploaded_file.name}", df.columns)
         dataframes[uploaded_file.name] = (df, selected_columns)
 
-        # Rename line names and set offsets
+        # Set offsets for each line
         for col in selected_columns:
-            default_name = f"{uploaded_file.name} - {col}"
-            line_name = st.text_input(f"Rename line for {col} in {uploaded_file.name}", value=default_name)
-            if line_name:
-                line_names.setdefault(uploaded_file.name, {})[col] = line_name
-
-            # Offsets
             col1, col2 = st.columns(2)
             with col1:
-                x_offset = st.number_input(f"Set X Offset for {col} in {uploaded_file.name}", value=0)
+                x_offset = st.slider(f"Set X Offset for {col} in {uploaded_file.name}", min_value=-100, max_value=100, value=0, key=f"x_{uploaded_file.name}_{col}")
             with col2:
-                y_offset = st.number_input(f"Set Y Offset for {col} in {uploaded_file.name}", value=0)
+                y_offset = st.slider(f"Set Y Offset for {col} in {uploaded_file.name}", min_value=-100, max_value=100, value=0, key=f"y_{uploaded_file.name}_{col}")
             
-            offsets.setdefault(uploaded_file.name, {}).setdefault(col, {})['x'] = x_offset
-            offsets.setdefault(uploaded_file.name, {}).setdefault(col, {})['y'] = y_offset
+            offsets[(uploaded_file.name, col)] = {'x': x_offset, 'y': y_offset}
 
     if st.button("Plot Data"):
-        fig = plot_data(dataframes, line_names, offsets)
+        fig = plot_data(dataframes, offsets)
         st.plotly_chart(fig, use_container_width=True)
 
     # Save, Print, Reset Buttons
